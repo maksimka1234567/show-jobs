bb82eac4bce8e36a0b4ea23d905ab8635aa5ccb5
levondra <lev.brodsky010@gmail.com>
2025-03-17 18:30:36 +0400

    authorization

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user
+from data import db_session
+from data.users import User
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
